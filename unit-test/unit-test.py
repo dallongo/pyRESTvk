160e0fb04bc840557fed7d1590532bfcc76a2b27
Dan Allongo <daniel.s.allongo@gmail.com>
2015-10-05 18:27:51 -0600

    update unit-test to use new file upload
    update profile format

--- unit-test/unit-test.py
+++ unit-test/unit-test.py
@@ -15,29 +15,20 @@ import copy
 server_ip = '127.0.0.1'
 server_port = 5000
 # testing for this API version
-api_version = '1.1'
+api_version = '2.0'
 # pass hostname as username
 username = platform.node()
-# get password from key file
-f = open('.auth_key')
-password = f.readline().rstrip()
-f.close()
-# set correct request headers
-headers = {
-	'X-Requested-With':'XMLHttpRequest',
-	'Accept':'application/json',
-	'Content-Type':'application/json'
-}
-# get test configuration
-f = open('unit-test.json')
-test_config = json.loads(f.read())
-f.close()
+# http password
+password = ''
+test_profile_file = 'unit-test.json'
+with open(test_profile_file) as f:
+	test_profile = json.load(f)
 # copy to test rename functionality
-test_config_2 = copy.deepcopy(test_config)
-test_config_2['name'] += ' 2'
+k, p = next(test_profile.iteritems())
+test_profile_2 = {k+' 2':p}
 # copy to test syntax verification
-test_config_bad = copy.deepcopy(test_config)
-test_config_bad['macros'][0]['keys'] += ' ['
+test_profile_bad = copy.deepcopy(test_profile)
+test_profile_bad[test_profile.keys()[0]]['alt-tab'] += ' ['
 # assume the server is local and on default port
 base_url = 'http://' + server_ip + ':' + str(server_port)
 # verify server API version
@@ -48,10 +39,9 @@ assert r.json()['application']['api'] == api_version
 r = requests.get(base_url + '/shutdown')
 assert r.status_code == 401
 print r.json()['message']
-# session will send auth and headers for subsequent requests
+# session will send auth for subsequent requests
 s = requests.Session()
 s.auth=(username,password)
-s.headers.update(headers)
 # test authentication and client registration
 r = s.get(base_url + '/auth')
 assert r.status_code == 200
@@ -66,129 +56,120 @@ r = s.get(clients_url)
 assert r.status_code == 200
 print r.text
 # find test client in list
-client = filter(lambda x: x['name'] == username, r.json()['clients'])
-assert client
-client = client.pop()
-# verify individual client URL
-r = s.get(client['url'])
-assert r.status_code == 200
-# get server status for configs URL
+assert username in r.json()
+# get server status for profiles URL
 r = s.get(base_url)
 assert r.status_code == 200
-# verify server provided correct URL for configs resource
-configs_url = r.json()['configs']['url']
-r = s.get(configs_url)
+# verify server provided correct URL for profiles resource
+profiles_url = r.json()['profiles']['url']
+r = s.get(profiles_url)
 assert r.status_code == 200
 print r.text
-# find and remove testing config if it exists
-config = filter(lambda x: x['name'] == test_config['name'], r.json()['configs'])
-if config:
-	config = config.pop()
-	r = s.delete(config['url'])
+# find and remove testing profile if it exists
+profiles = r.json()
+if test_profile.keys()[0] in profiles:
+	r = s.delete(profiles[test_profile.keys()[0]]['url'])
 	assert r.status_code == 204
 # repeat request since resource list has possibly changed
-r = s.get(configs_url)
+r = s.get(profiles_url)
 assert r.status_code == 200
-# find and delete testing config if it exsists
-config = filter(lambda x: x['name'] == test_config_2['name'], r.json()['configs'])
-if config:
-	config = config.pop()
-	r = s.delete(config['url'])
+# find and delete testing profile if it exsists
+profiles = r.json()
+if test_profile_2.keys()[0] in profiles:
+	r = s.delete(profiles[test_profile_2.keys()[0]]['url'])
 	assert r.status_code == 204
 # verify PUT not allowed
-r = s.put(configs_url)
+r = s.put(profiles_url)
 assert r.status_code == 405
 # verify DELETE not allowed
-r = s.delete(configs_url)
+r = s.delete(profiles_url)
 assert r.status_code == 405
-# verify config syntax validation
-r = s.post(configs_url, json=test_config_bad, params={'validate_only':'true'})
+# verify profile syntax validation
+r = s.post(profiles_url, json=test_profile_bad, params={'validate_only':'true'})
 assert r.status_code == 400
 print r.json()['message']
-# create new test config
-r = s.post(configs_url, json=test_config)
+# create new test profile via uploaded file
+r = s.post(profiles_url, files={test_profile_file : open(test_profile_file, 'rb')})
 assert r.status_code == 201
 print r.text
 # verify server returned valid location URL header for new resource
-config_url = r.headers['location']
-r = s.get(config_url)
+profile_url = r.headers['location']
+r = s.get(profile_url)
 assert r.status_code == 200
 print r.text
-assert test_config == r.json()
+assert test_profile == r.json()
 # verify POST not allowed
-r = s.post(config_url)
+r = s.post(profile_url)
 assert r.status_code == 405
 # verify single combo macro functionality
-r = s.get(config_url + '/macros/run-dialog')
+r = s.get(profile_url + '/run-dialog')
 assert r.status_code == 200
 # visually verify run dialog is open
 time.sleep(0.25)
 # verify typing macro functionality
-r = s.get(config_url + '/macros/type notepad')
+r = s.get(profile_url + '/type notepad')
 assert r.status_code == 200
 # visually verify notepad is open
 time.sleep(0.25)
 # verify combo + typing macro functionality
-r = s.get(config_url + '/macros/type sentence')
+r = s.get(profile_url + '/type sentence')
 assert r.status_code == 200
 # visually verify output
 time.sleep(0.25)
-r = s.get(config_url + '/macros/cut-paste')
+r = s.get(profile_url + '/cut-paste')
 assert r.status_code == 200
 # visually verify output
 time.sleep(0.25)
 # exit notepad
-r = s.get(config_url + '/macros/file-menu')
+r = s.get(profile_url + '/file-menu')
 assert r.status_code == 200
 # attempt duplicate create
-r = s.post(configs_url, json=test_config)
+r = s.post(profiles_url, json=test_profile)
 assert r.status_code == 409
 print r.json()['message']
-# update config same name
-r = s.put(config_url, json=test_config)
+# update profile same name
+r = s.put(profile_url, json=test_profile)
 assert r.status_code == 204
-# verify config syntax validation on update
-r = s.put(config_url, json=test_config_bad)
+# verify profile syntax validation on update
+r = s.put(profile_url, json=test_profile_bad)
 assert r.status_code == 400
 print r.json()['message']
-# verify rename config via update
-r = s.put(config_url, json=test_config_2)
+# verify rename profile via update
+r = s.put(profile_url, json=test_profile_2)
 assert r.status_code == 201
-config_url = r.headers['location']
+profile_url = r.headers['location']
 # verify original resource name is now available
-r = s.post(configs_url, json=test_config)
+r = s.post(profiles_url, json=test_profile)
 assert r.status_code == 201
 # attempt rename to existing resource
-r = s.put(config_url, json=test_config)
+r = s.put(profile_url, json=test_profile)
 assert r.status_code == 409
 print r.json()['message']
-# delete test config 2
-r = s.delete(config_url)
+# delete test profile 2
+r = s.delete(profile_url)
 assert r.status_code == 204
 # confirm not found at individual URL
-r = s.get(config_url)
+r = s.get(profile_url)
 assert r.status_code == 404
-# confirm not found in configs resource list
-r = s.get(configs_url)
+# confirm not found in profiles resource list
+r = s.get(profiles_url)
 assert r.status_code == 200
-config = filter(lambda x: x['name'] == test_config_2['name'], r.json()['configs'])
-assert not config
-# delete test config
-r = s.get(configs_url)
+assert test_profile_2.keys()[0] not in r.json()
+# delete test profile
+r = s.get(profiles_url)
 assert r.status_code == 200
-config = filter(lambda x: x['name'] == test_config['name'], r.json()['configs'])
-assert config
-config = config.pop()
-r = s.delete(config['url'])
+profiles = r.json()
+assert test_profile.keys()[0] in profiles
+r = s.delete(profiles[test_profile.keys()[0]]['url'])
 assert r.status_code == 204
 # confirm not found at individual URL
-r = s.get(config['url'])
+r = s.get(profiles[test_profile.keys()[0]]['url'])
 assert r.status_code == 404
-# confirm not found in configs resource list
-r = s.get(configs_url)
+# confirm not found in profiles resource list
+r = s.get(profiles_url)
 assert r.status_code == 200
-config = filter(lambda x: x['name'] == test_config['name'], r.json()['configs'])
-assert not config
+profiles = r.json()
+assert test_profile.keys()[0] not in profiles
 # server shutdown via authenticated endpoint
 r = s.get(base_url + '/shutdown')
 assert r.status_code == 200
